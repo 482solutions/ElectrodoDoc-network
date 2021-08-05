fcfce6cea22c14b601cd7a0f2552cd8b3a60eafc
Andrii Spivak <andrii.spivak@482.solutions>
2021-08-05 17:13:18 +0300

    addsome whitespaces for more comfortable reading

--- deploy.sh
+++ deploy.sh
@@ -1,75 +1,147 @@
 #!/bin/bash
+#
 docker network rm front_default
+#
 docker network create --driver=bridge  --subnet=172.28.0.0/16  --ip-range=172.28.0.0/24   --gateway=172.28.0.254  front_default
+#
+#############################################
+#
 echo -----Run Fabric CA Server
+#
 cd ./network/ca
+#
 docker-compose up -d
+#
 cd ../../
+#
+#############################################
+#
 echo -----Enroll admin msp
+#
 docker run --rm --network front_default --name fabric_ca_client \
 -e "FABRIC_CA_HOME=/etc/hyperledger/fabric-ca-server" \
 -v $(pwd)/data:/etc/hyperledger/fabric-ca-server hyperledger/fabric-ca:1.4 \
 sh -c 'sleep 5 && fabric-ca-client enroll --url http://admin:password@172.28.0.3:7054'
+#############################################
+#
 echo -----Backup admin msp
+#
 mkdir -p ./admin/msp
+#
 cp -r ./data/msp ./admin/
+#
+#############################################
+#
 echo -----Create node account
+#
 docker run --rm --network front_default --name fabric_ca_client \
 -e "FABRIC_CA_HOME=/etc/hyperledger/fabric-ca-server" \
 -v $(pwd)/data:/etc/hyperledger/fabric-ca-server hyperledger/fabric-ca:1.4 \
 sh -c 'sleep 5 && fabric-ca-client register --id.name peer1 --id.affiliation 482solutions.prj-fabric --id.secret passwd --id.type peer'
+#
+#############################################
+#
 echo -----Enroll node account
+#
 docker run --rm --network front_default --name fabric_ca_client \
 -e "FABRIC_CA_HOME=/etc/hyperledger/fabric-ca-server" \
 -v $(pwd)/data:/etc/hyperledger/fabric-ca-server hyperledger/fabric-ca:1.4 \
 sh -c 'sleep 5 && fabric-ca-client enroll -u http://peer1:passwd@172.28.0.3:7054'
+#
+#############################################
+#
 echo -----Build node MSP
+#
 cp -r ./data/msp ./network/peer/data/
+#
 mkdir -p ./network/peer/data/msp/admincerts
+#
 cp ./admin/msp/signcerts/cert.pem ./network/peer/data/msp/admincerts/
+#
 cp ./network/msp/config.yaml ./network/peer/data/msp/
 # Change values in all Certificate fields of ./data/msp/config.yaml to the actual name of the certificate in ./data/msp/cacerts
 # Change peer settings
 # In ./data/core.yaml change the following settings:
 # peer.address to the actual IP address of your node server
 # If --id.name in "Create node account" was different from the provided in the tutorial, change peer.id to the actual peer name.
+#############################################
+#
 echo -----Run Fabric Peer Node
+#
 cd ./network/peer
+#
 docker-compose up -d
+#
 cd ../../
+#
+#############################################
+#
 echo ----Update admin msp
+#
 rm -rf ./data/msp
+#
 cp -r ./admin/msp ./data/
+#
+#############################################
+#
 echo -----Create node account
+#
 docker run --rm --network front_default --name fabric_ca_client \
 -e "FABRIC_CA_HOME=/etc/hyperledger/fabric-ca-server" \
 -v $(pwd)/data:/etc/hyperledger/fabric-ca-server hyperledger/fabric-ca:1.4 \
 sh -c 'sleep 5 && fabric-ca-client register --id.name orderer --id.affiliation 482solutions.prj-fabric --id.secret passwd --id.type peer'
+#
+#############################################
+#
 echo -----Enroll node account
+#
 docker run --rm --network front_default --name fabric_ca_client \
 -e "FABRIC_CA_HOME=/etc/hyperledger/fabric-ca-server" \
 -v $(pwd)/data:/etc/hyperledger/fabric-ca-server hyperledger/fabric-ca:1.4 \
 sh -c 'sleep 5 && fabric-ca-client enroll -u http://orderer:passwd@172.28.0.3:7054'
+#
+#############################################
+#
 echo -----Build orderer MSP
+#
 cp -r ./data/msp ./network/orderer/data/
+#
 mkdir -p ./network/orderer/data/msp/admincerts
+#
 cp ./admin/msp/signcerts/cert.pem ./network/orderer/data/msp/admincerts/
+#
 cp ./network/msp/config.yaml ./network/orderer/data/msp/
 # Change values in all Certificate fields of ./data/msp/config.yaml to the actual name of the certificate in ./data/msp/cacerts
 # Change orderer settings
 # In ./data/configtx.yaml change the following:
 # In Organizations[482solutions].AnchorPeers list the IP address of the previously created peer.
 # In Profiles.SampleSingleMSPSolo.Orderer.Addresses fill in the IP address of the orderer service.
+#############################################
+#
 echo -----Run Fabric Peer Node
+#
 cd ./network/orderer
+#
 docker-compose up -d
+#
 cd ../../
+#
+#############################################
+#
 echo ----Update admin msp
+#
 rm -rf ./data/msp
+#
 cp -r ./admin/msp ./data/
+#
+#############################################
+#
 echo ----Change admin MSP
+#
 mkdir -p ./data/msp/admincerts
+#
 cp ./admin/msp/signcerts/cert.pem ./data/msp/admincerts
+#
 docker run --rm --network front_default --name cli \
 -e "GOPATH=/opt/gopath" \
 -e "CORE_VM_ENDPOINT=unix:///host/var/run/docker.sock" \
